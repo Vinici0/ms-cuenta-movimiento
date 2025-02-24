2f68896c9cac163760c48ef177d030570e149612
vinicio <vinicio.borja10@gmail.com>
2025-02-24 02:51:35 -0500

    Remove Kafka integration from AccountService: delete KafkaTemplate usage and related properties from application configuration

--- src/main/java/org/borja/springcloud/msvc/account/services/account/AccountService.java
+++ src/main/java/org/borja/springcloud/msvc/account/services/account/AccountService.java
@@ -1,30 +1,19 @@
 package org.borja.springcloud.msvc.account.services.account;
 
-// Java core imports
+
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.header.internals.RecordHeader;
+
 import org.borja.springcloud.msvc.account.dtos.account.AccountRequestDto;
 import org.borja.springcloud.msvc.account.dtos.account.AccountResponseDto;
 import org.borja.springcloud.msvc.account.exceptions.ResourceNotFoundException;
 import org.borja.springcloud.msvc.account.models.Account;
-import org.borja.springcloud.msvc.account.models.Client;
+
 import org.borja.springcloud.msvc.account.repositories.AccountRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -33,28 +22,10 @@ public class AccountService implements IAccountService {
     private static final Logger log = LoggerFactory.getLogger(AccountService.class);
     private final AccountRepository accountRepository;
 
-    @Value("${topic}")
-    String topic;
-
-    @Autowired
-    KafkaTemplate<String, Client> kafkaTemplate;
-
     @Override
     public Mono<AccountResponseDto> addAccount(AccountRequestDto accountDto) {
         log.info("Adding new account for client ID: {}", accountDto.getClientId());
 
-        Client client = new Client();
-        client.setId(accountDto.getClientId());
-        CompletableFuture<SendResult<String, Client>> future = kafkaTemplate.send(topic, client);
-
-        future.whenCompleteAsync((result, ex) -> {
-            if (ex != null) {
-                log.error("Error sending message: {}", ex.getMessage());
-                throw new RuntimeException(ex);
-            } else {
-                log.info("Message sent: {}", result.getProducerRecord().value());
-            }
-        });
         Account account = new Account();
         account.setAccountType(accountDto.getAccountType());
         account.setInitialBalance(accountDto.getInitialBalance());
