c4212d08004ba9aa5300795f93cb4d26924a8b5a
vinicio <vinicio.borja10@gmail.com>
2025-03-09 22:48:41 -0500

    Update .gitignore to include README_MEJORAS.md and clean up AccountApplication class

--- src/main/java/org/borja/springcloud/msvc/account/AccountApplication.java
+++ src/main/java/org/borja/springcloud/msvc/account/AccountApplication.java
@@ -7,9 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
-
-
-
-
     }
 }
